32a2d6becd46ed364d509e2462ea0b058e2e489e
pthoang <phantichhoang@gmail.com>
2024-10-19 08:52:27 +0700

    fix: another files

---
+++ Demo/calculator/test/incremental.test.js
@@ -0,0 +1,36 @@
+require('../api/helpers');
+const { isIncremental } = require('../api/controller');
+
+describe('isIncremental Function', () => {
+  it('returns true for a string of 4 incremental digits', () => {
+    expect(isIncremental('1234')).to.eql(true);
+  });
+
+  it('returns true for a string of more than 4 incremental digits', () => {
+    expect(isIncremental('12345')).to.eql(true);
+  });
+
+  it('returns false for a string of less than 4 digits', () => {
+    expect(isIncremental('123')).to.eql(false);
+  });
+
+  it('returns false for a string with non-incremental digits', () => {
+    expect(isIncremental('1245')).to.eql(false);
+  });
+
+  it('returns false for a string with non-digit characters', () => {
+    expect(isIncremental('12a4')).to.eql(false);
+  });
+
+  it('returns false for an empty string', () => {
+    expect(isIncremental('')).to.eql(false);
+  });
+
+  it('returns false for a string with negative digits', () => {
+    expect(isIncremental('-1234')).to.eql(false);
+  });
+
+  it('returns false for a string with spaces', () => {
+    expect(isIncremental('1 234')).to.eql(false);
+  });
+});
